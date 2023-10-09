7057f2168af52f6c75b7641321416fabb1a08a7b
´cocuxd23´ <lautaroagsalina15@gmail.com>
2023-10-09 01:32:59 -0300

    juntada

--- src/test/java/GestorCorreo/EnviarMailsTest.java
+++ src/test/java/GestorCorreo/EnviarMailsTest.java
@@ -9,17 +9,21 @@ public class EnviarMailsTest {
 @Test
     public void create_app_create_user_send_mail(){
     MailManager aplicacion = new MailManager();
-    User lau = aplicacion.crearUsuario("Lautaro", "Vegano", "lauvega");
-    User felix = aplicacion.crearUsuario("Felix", "Toledo", "felixt");
-    User pedro = aplicacion.crearUsuario("Pedro", "Rosales", "pedro@gmail.com");
+    User ori = aplicacion.crearUsuario("Oriana", "Farela", "orifarela");
+    User paulina = aplicacion.crearUsuario("Paulina", "Palmeyro", "pauliyro");
+    User gonza = aplicacion.crearUsuario("Gonza", "Cabral", "gonza@gmail.com");
     ArrayList<String> para = new ArrayList<>();
-    para.add("felixt@aquilita.com");
-    para.add("pedro@gmail.com");
+    para.add("pauliyro@aquilita.com");
+    para.add("gonza@gmail.com");
 
-    lau.crearMensaje(aplicacion, "Que onda mi hermano", "Buennos dias, quiero pregunntarte como estas", para);
-    assertEquals(1, lau.getSalida().tamano());
-    assertEquals(1, felix.getEntrada().tamano());
-    assertEquals(1, pedro.getEntrada().tamano());
+    //User lau = aplicacion.crearUsuario("Lautaro", "Vegano", "lauvega");
+    //User felix = aplicacion.crearUsuario("Felix", "Toledo", "felixt");
+   //User pedro = aplicacion.crearUsuario("Pedro", "Rosales", "pedro@gmail.com");
+
+    ori.crearMensaje(aplicacion, "Juntada", "Buennos dias, estan para juntarse mañana?", para);
+    assertEquals(1, ori.getSalida().tamano());
+    assertEquals(1, paulina.getEntrada().tamano());
+    assertEquals(1, gonza.getEntrada().tamano());
 }
 
 }
